f73c3b10a113b1b4ca6868fc3b3f00af52c767b5
vincent munywoki <vincentmunywoki12@gmail.com>
2025-02-06 16:56:36 +0300

    connecting routes

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
-});
+// Middleware
+app.use(cors());
+app.use(express.json());
 
+// Connecting to MongoDB
+mongoose
+  .connect('your-mongodb-atlas-connection-string', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+// Use auth routes
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
