ea33df25b314601236bf2e7c78e2b8b690bfceab
vincent munywoki <vincentmunywoki12@gmail.com>
2025-02-06 16:35:42 +0300

    basic server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
